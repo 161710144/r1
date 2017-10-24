ad57f72c6ad24929f8c568cb4e8d64b0390920fa
unknown <161710116@smkassalaambandung.sch.id>
2017-10-24 17:03:28 +0700

    latihan 1

---
+++ t1.php
@@ -0,0 +1,16 @@
+<?php
+class bio{
+public $nama;
+public $nis;
+public $alamat;
+public $tempat_lahir;
+
+public function __construct($nama,$nis,$alamat,$tempat_lahir){
+$this->nama = $nama;
+$this->nis = $nis;
+$this->alamat = $alamat;
+$this->tempat_lahir = $tempat_lahir;
+}
+}
+
+?>
---
+++ t2.php
@@ -0,0 +1,11 @@
+<center>
+<?php
+require_once't1.php';
+
+$bio1 = new bio("Ukri","171790","Jl.Sudirja","Bogor");
+echo "Namanya adalah ".$bio1->nama.'<br>';
+echo "nisnya terdiri dari ".$bio1->nis.'<br>';
+echo "Alamatnya di ".$bio1->alamat.'<br>';
+echo "Melahirkan di ".$bio1->tempat_lahir.'<br>';
+?>
+</center>
